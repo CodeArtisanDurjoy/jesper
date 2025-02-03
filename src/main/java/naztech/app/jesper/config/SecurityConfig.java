d5450c969ec64dcf9eb603d42eff332d76922ae7
Durjoy Acharjya <da-durjoy@outlook.com>
2025-02-03 16:40:16 +0600

    Update SecurityConfig.java

--- src/main/java/naztech/app/jesper/config/SecurityConfig.java
+++ src/main/java/naztech/app/jesper/config/SecurityConfig.java
@@ -107,7 +107,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:9090"));
+        configuration.setAllowedOrigins(Arrays.asList("https://jesper-production.up.railway.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
